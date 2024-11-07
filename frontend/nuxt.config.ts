99d4308478aa60d0a5f60818b63e0897ce60e032
Andy @ Personal <andywillekens@gmail.com>
2024-11-07 19:57:01 +0100

    Added tailwind, vueuse, icons and fonts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,15 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4
   },
+
   runtimeConfig: {
     public: {
       API_URL:
         `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}` || 'http://localhost:8080'
     }
   },
+
   router: { options: { strict: true } },
-  srcDir: 'src/'
+  srcDir: 'src/',
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxt/fonts', '@nuxt/icon']
 })
